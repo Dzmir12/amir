53996e17e184907944d1ac68d372b6468dcc516d
amir12 <dzmir12@gmail.com>
2023-11-11 08:57:07 -0800

    1st commit

--- my-resume/src/App.jsx
+++ my-resume/src/App.jsx
@@ -1,42 +1,22 @@
-// import Skills from "./components/Skills/Skills";
-// import Header from "./components/Header/Header";
-// import AboutME from "./components/AboutMe/AboutMe";
-// import LazyLoad from "react-lazyload";
-// import "./App.css";
-// import Contact from "./components/Contact/Contact";
-
-// export default function App() {
-//   return (
-//     <div className="app">
-
-//      <Header/>
-//      <AboutME />
-//      <Skills />
-//      <Contact />
-     
-//     </div>
-//   );
-// }
-
-import React, { Suspense } from 'react';
-import LazyLoad from 'react-lazyload';
-
-// Wrap components in Suspense with LazyLoad for dynamic loading
-const LazySkills = React.lazy(() => import('./components/Skills/Skills'));
-const LazyHeader = React.lazy(() => import('./components/Header/Header'));
-const LazyAboutMe = React.lazy(() => import('./components/AboutMe/AboutMe'));
-const LazyContact = React.lazy(() => import('./components/Contact/Contact'));
+import Skills from "./components/Skills/Skills";
+import Header from "./components/Header/Header";
+import AboutME from "./components/AboutMe/AboutMe";
+import LazyLoad from "react-lazyload";
+import "./App.css";
+import Contact from "./components/Contact/Contact";
 
 export default function App() {
   return (
     <div className="app">
-      <Suspense fallback={<div>Loading...</div>}>
-        <LazyHeader />
-        <LazyAboutMe />
-        <LazySkills />
-        <LazyContact />
-      </Suspense>
+
+     <Header/>
+     <AboutME />
+     <Skills />
+     <Contact />
+     
     </div>
   );
 }
+
+
 
